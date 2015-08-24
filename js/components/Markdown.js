fe33658907afc8b26835211ec585ae589e6595bf
Joe Lencioni <joe.lencioni@brigade.com>
2015-08-23 17:36:46 -0700

    Link @mentions in <Markdown>
    People use this convention to mention other people. Since we don't have
    user pages in this app yet, I'm just linking to their GitHub profiles.

--- js/components/Markdown.js
+++ js/components/Markdown.js
@@ -2,10 +2,18 @@ const marked = require('marked');
 
 export default class Markdown extends React.Component {
   render() {
+    // We want to link up any @username mention to that person's GitHub profile.
+    // Since this happens before we convert the markdown to HTML, we can just
+    // use markdown stynax.
+    const augmentedSource = this.props.source.replace(
+      // \b doesn't work with @, so we need to match on either the beginning of
+      // the string or whitespace.
+      /(^|\s)@(\w+)/, '$1[@$2](https://github.com/$2)');
+
     return (
       <span
         className='Markdown'
-        dangerouslySetInnerHTML={{ __html: marked(this.props.source) }}
+        dangerouslySetInnerHTML={{ __html: marked(augmentedSource) }}
       />
     );
   }
